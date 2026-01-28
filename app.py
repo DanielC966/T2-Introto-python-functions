43d80d13e080c911663add624e6c12298aadcbda
DanielC966 <danielc966@nycstudents.net>
2026-01-28 09:23:10 -0500

    initial daty

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+
+def square(x):
+    for i in range(4):
+        t.forward(x)
+        t.left(90)
+def invsquare(x):
+    for i in range(4):
+    t.forward(x)
+    t.right(90)
+def backsquare(x):
+    for i in range(4):
+    t.forward(x)
+    t.right(90)  
+
+square(200)
+invsquare(200)
+t.shapesize(5, 7, 8)
+
+
+turtle.done()
